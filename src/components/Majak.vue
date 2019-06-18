604455c5638b9ce496596305f80d72e3b7d3730f
Lukáš Pokorný <luk4s.pokorny@gmail.com>
2019-06-18 23:02:15 +0200

    move background

--- src/components/Majak.vue
+++ src/components/Majak.vue
@@ -1,27 +1,29 @@
 <template>
   <div id="majak" class="text-center">
-    <h1>Rozhledna Bežerovice
-      <small class="text-muted">– nejvyšší výhled v České republice</small>
-    </h1>
+    <header>
+      <h1>Rozhledna Bežerovice
+        <small class="text-muted">– nejvyšší výhled v České republice</small>
+      </h1>
 
-    <p class="lead">
-      První bezbariérová interaktivní rozhledna s možností cestování v čase. Zde Vás neohrozí ani mlha, ani déšť
-    </p>
-
+      <p class="lead">
+        První bezbariérová interaktivní rozhledna s možností cestování v čase. Zde Vás neohrozí ani mlha, ani déšť
+      </p>
+    </header>
     <div class="row">
       <div v-for="item in sources" v-bind:key="item.id" class="col-6 majak-view">
-          <div class="position-static m-4 p-2 video-wrapper">
-            <h5 class="mt-0">{{ item.title }}</h5>
-            <video :src="'/videos/' + item.filename + '.MP4'" :poster="'/videos/thumbnails/' + item.filename + '.png'"
-                   width="480"></video>
-            <PlayVideo></PlayVideo>
-          </div>
+        <div class="position-static m-4 p-2 video-wrapper">
+          <h5 class="mt-0">{{ item.title }}</h5>
+          <video :src="'/videos/' + item.filename + '.MP4'" :poster="'/videos/thumbnails/' + item.filename + '.png'"
+                 width="480"></video>
+          <PlayVideo></PlayVideo>
+        </div>
       </div>
     </div>
 
     <p>
       <b>
-        Vstup na vlastní nebezpečí. Provozovatel nenese odpovědnost za zranění při výstupu a sestupu ani za závratě a jiné psychické potíže způsobené použitím rozhledny.
+        Vstup na vlastní nebezpečí. Provozovatel nenese odpovědnost za zranění při výstupu a sestupu ani za závratě a
+        jiné psychické potíže způsobené použitím rozhledny.
       </b>
     </p>
   </div>
